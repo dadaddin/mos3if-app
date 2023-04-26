e0858ba0952e48debd195038e0d8b3c7f3bd0c09
dadaddin <dhiyaazzouz@gmail.com>
2023-04-27 00:22:41 +0100

    few changes

---
+++ app/src/main/java/com/example/mos3if/MainScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.mos3if;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
---
+++ app/src/main/java/com/example/mos3if/OnboardingActivity.java
@@ -0,0 +1,130 @@
+package com.example.mos3if;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class OnboardingActivity extends AppCompatActivity {
+    ViewPager mSLideViewPager;
+    LinearLayout mDotLayout;
+    Button  nextbtn, skipbtn;
+
+    TextView[] dots;
+    ViewPagerAdapterActivity viewPagerAdapter;
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        nextbtn = findViewById(R.id.nextbtn);
+        skipbtn = findViewById(R.id.skipbtn);
+
+
+
+
+        nextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (getitem(0) < 2)
+                    mSLideViewPager.setCurrentItem(getitem(1),true);
+                else {
+
+                    Intent i = new Intent(OnboardingActivity.this, MainScreenActivity.class);
+                    startActivity(i);
+                    finish();
+
+                }
+
+            }
+        });
+
+        skipbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent i = new Intent(OnboardingActivity.this, MainScreenActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
+
+        mSLideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.dotIndicator);
+
+        viewPagerAdapter = new ViewPagerAdapterActivity(this);
+
+        mSLideViewPager.setAdapter(viewPagerAdapter);
+
+        setUpindicator(0);
+        mSLideViewPager.addOnPageChangeListener(viewListener);
+
+    }
+
+    public void setUpindicator(int position){
+
+        dots = new TextView[3];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0 ; i < dots.length ; i++){
+
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226", Html.FROM_HTML_MODE_LEGACY));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.white,getApplicationContext().getTheme()));
+            mDotLayout.addView(dots[i]);
+
+        }
+
+        dots[position].setTextColor(getResources().getColor(R.color.yellow,getApplicationContext().getTheme()));
+
+    }
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            setUpindicator(position);
+
+
+
+            if (position == 2){
+                nextbtn.setText("Finish");
+            } else {
+                nextbtn.setText("Next");
+            }
+
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private int getitem(int i){
+
+        return mSLideViewPager.getCurrentItem() + i;
+    }
+
+}
